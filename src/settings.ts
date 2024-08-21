8dadd8c419148cbef8580aee53540fb0a151fb39
Luke Winikates <luke.winikates@gmail.com>
2024-08-20 17:46:16 -0700

    fix: correct the plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'SleepMeHomebridgePlugin';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@lukewinkates/homebridge-sleepme';
+export const PLUGIN_NAME = 'homebridge-sleepme';
