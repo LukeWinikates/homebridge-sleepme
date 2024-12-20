0eff663b286035645c01f56271878231748a7acb
Luke Winikates <luke.winikates@gmail.com>
2024-12-20 13:20:19 -0800

    wip

---
+++ src/platformAccessory.test.ts
@@ -0,0 +1,44 @@
+import {describe, expect, test, jest} from '@jest/globals';
+import {PluginConfig, SleepmePlatform} from "./platform";
+import {HomebridgeAPI} from "homebridge/lib/api";
+import {Logger} from "homebridge/lib/logger";
+import EventEmitter from "events";
+
+// function createPluginForTest(config: unknown): SleepmePlatform {
+//   const api = new HomebridgeAPI()
+//   const logger = Logger.withPrefix("test")
+//   jest.spyOn(logger, "error").mockImplementation(() => {
+//   });
+//   return new SleepmePlatform(logger, config as PluginConfig, api);
+// }
+
+describe('platformAccessory', () => {
+
+  // read-through cache
+  // on first get, make API call
+  // on second get with time window,
+  // on get outside of time window,
+
+
+  // polling -> if it's on standby, poll every 15m
+  // if it's actively changing, poll every 1.5s?
+
+
+  // how to handle rate limiting?
+  // log a warning
+  // reduce polling frequency
+  // bubble up an error on the accessory somehow?
+
+  describe("startup", () => {
+    test('with no api_keys', () => {
+      const platform = createPluginForTest({platform: "SleepmeHomebridgePlugin"});
+      (platform.api as unknown as EventEmitter).emit("didFinishLaunching");
+      expect(platform.log.error).toHaveBeenCalledWith("No API keys configured - plugin will not start")
+    })
+    test('with non-array api_keys', () => {
+      const platform = createPluginForTest({platform: "SleepmeHomebridgePlugin", api_keys: {a: "b"}});
+      (platform.api as unknown as EventEmitter).emit("didFinishLaunching");
+      expect(platform.log.error).toHaveBeenCalledWith("No API keys configured - plugin will not start")
+    })
+  })
+});
