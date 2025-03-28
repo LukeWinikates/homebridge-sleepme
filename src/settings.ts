fe18d68b91b1d74dad9352915ba089a256843287
Dave <22969871+DaveLinger@users.noreply.github.com>
2025-03-28 13:28:52 -0400

    Revert "Merge branch 'main' of https://github.com/DaveLinger/homebridge-sleepme-dockpro"
    This reverts commit 92f470c13b24cb64d53b27545acabade37c3eb5b, reversing
    changes made to 21b68664531a24f2c38445dadf43d253e646d3eb.

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'SleepmeDockProHomebridgePlugin';
+export const PLATFORM_NAME = 'SleepmeHomebridgePlugin';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-sleepme-dockpro';
+export const PLUGIN_NAME = 'homebridge-sleepme';
